17f741f99ce2e8c6700e6f8e13c160287e80ff40
Vivian Pinheiro <vivianpinheirom@gmail.com>
2023-08-10 23:52:03 -0300

    Create codepark01.js

---
+++ logica/m3/codepark01.js
@@ -0,0 +1,38 @@
+const nomes = [
+    "Ana",
+    "Carlos",
+    "Mariana",
+    "João",
+    "Isabela",
+    "Pedro",
+    "Sofia",
+    "Lucas",
+    "Laura",
+    "Gustavo"
+];
+
+const idades = [25, 32, 20, 28, 24, 30, 22, 29, 27, 23];
+
+const coresFavoritas = [
+    "azul",
+    "vermelho",
+    "verde",
+    "amarelo",
+    "roxo",
+    "laranja",
+    "rosa",
+    "preto",
+    "branco",
+    "cinza"
+];
+
+console.log("Lista de nomes:", nomes);
+console.log("Lista de idades:", idades);
+console.log("Lista de cores favoritas:", coresFavoritas);
+
+idades[2] = 21; 
+coresFavoritas[7] = "azul"; 
+
+console.log("Lista de nomes:", nomes);
+console.log("Lista de idades:", idades);
+console.log("Lista de cores favoritas:", coresFavoritas);
